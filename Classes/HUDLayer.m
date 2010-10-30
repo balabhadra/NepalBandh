cda368dd9a1fef65f98902148a59d3314cf04034
Rajan <rajan@sproutify.com>
2010-10-30 09:32:28 +0545

    Added sound files and Game Timer

--- Classes/HUDLayer.m
+++ Classes/HUDLayer.m
@@ -16,18 +16,26 @@
 
 @synthesize score;
 @synthesize gameLevel;
+@synthesize timeRemainingLabel;
 
 
 - (id)init	{
 	if ( (self = [super	init]) ) {
 		GameModel *game = [(NepalBandhAppDelegate *)[[UIApplication sharedApplication] delegate] game];
 		livesValue = game.lives;
+		timeValue = game.levelDuration;
+		
+		int margin = 5;
 		
 		CGSize winSize = [[CCDirector sharedDirector] winSize];
 		
 		CCSprite *hudImage = [CCSprite spriteWithFile:@"hud.png"];
 		hudImage.position = ccp(winSize.width / 2, winSize.height - hudImage.contentSize.height / 2);
-		[self addChild:hudImage z:0 tag:0];
+		[self addChild:hudImage z:0];
+
+		CCSprite *clockImage = [CCSprite spriteWithFile:@"clock.png"];
+		clockImage.position = ccp (clockImage.contentSize.width / 2 + margin, clockImage.contentSize.height + margin);
+		[self addChild:clockImage z:0];
 		
 		for (int i = 0; i < livesValue; i++) {
 			CCSprite *heart = [CCSprite spriteWithFile:@"heart.png"];
@@ -46,17 +54,23 @@
 									  alignment:UITextAlignmentRight 
 									   fontName:@"Trebuchet MS" fontSize:18.0];
 		
+		timeRemaingLabel = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"%d", game.levelDuration]
+											dimensions:CGSizeMake(20, 20)
+											 alignment:UITextAlignmentCenter
+											  fontName:@"Trebuchet MS" fontSize:18.0];
+		
 		
 		gameLevel.color = ccc3( 255, 255, 255);
 		score.color = ccc3(255, 255, 255);
+		timeRemaingLabel.color = ccc3(255, 255, 255);
 
-		int margin = 10;
-		
 		gameLevel.position = ccp(gameLevel.contentSize.width / 2 + margin + 50, winSize.height - gameLevel.contentSize.height / 2 - 2);
 		score.position = ccp(winSize.width / 2 - 50, winSize.height - score.contentSize.height / 2 - 2);		
+		timeRemaingLabel.position = ccp(timeRemaingLabel.contentSize.width / 2 + margin + 8, timeRemaingLabel.contentSize.height / 2 + 2);
 		
 		[self addChild:gameLevel z:1];
 		[self addChild:score z:1];
+		[self addChild:timeRemaingLabel z:1 tag:8];
 		
 		CCMenuItemImage *pauseMenuItem = [CCMenuItemImage itemFromNormalImage:@"pauseButton.png" 
 															   selectedImage:@"pauseButton_on.png"
@@ -65,8 +79,9 @@
 		CCMenu *menu = [CCMenu menuWithItems:pauseMenuItem, nil];
 		menu.position = ccp(winSize.width - pauseMenuItem.contentSize.width / 2, pauseMenuItem.contentSize.height / 2);
 		
-		[self addChild:menu z:1 tag:6];		
+		[self addChild:menu z:1 tag:6];
 		
+		[self schedule:@selector(updateTime:) interval:1];
 	}
 	return self;
 }
@@ -120,7 +135,7 @@
 		// code to increase live
 		if (livesValue < 5) {
 			currentLives = livesValue;	
-			if (currentLives == 0) return;
+			if (currentLives == 5) return;
 			livesValue++;
 			
 			NSLog(@"Update add Lives!");
@@ -147,6 +162,21 @@
 
 - (void)updateGameLevel:(NSInteger)level {
 	[gameLevel setString:[NSString stringWithFormat:@"%d", level]];
+}
+
+- (void)updateTime:(ccTime)dt {
+	if (timeValue != 0) {
+		timeValue--;
+		int margin = 5;
+		[self removeChild:[self getChildByTag:8] cleanup:YES];
+		timeRemaingLabel = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"%d", timeValue]
+										dimensions:CGSizeMake(20, 20)
+										 alignment:UITextAlignmentCenter
+										  fontName:@"Trebuchet MS" fontSize:18.0];
+		timeRemaingLabel.color = ccc3(255, 255, 255);
+		timeRemaingLabel.position = ccp(timeRemaingLabel.contentSize.width / 2 + margin + 8, timeRemaingLabel.contentSize.height / 2 + 2);
+		[self addChild:timeRemaingLabel z:1 tag:8];
+	}
 }
 
 @end
